f15ceb58c9020e70ee789888b00e278a2add0427
nlrarojas <nlrarojas@hotmail.com>
2017-05-06 15:06:51 -0600

    Prueba

---
+++ src/model/VideoProcessor.java
@@ -0,0 +1,31 @@
+package model;
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class VideoProcessor {
+
+    /**
+     * Default constructor
+     */
+    public VideoProcessor() {
+    }
+
+
+    /**
+     * 
+     */
+    public void sleep() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void getResultado() {
+        // TODO implement here
+    }
+
+}
