81a58070b529634198b6f0f2176d95a2009fddbe
nlrarojas <nlrarojas@hotmail.com>
2017-05-28 13:43:04 -0600

    Estructura del proyecto

--- src/library/Cuerpo.java
+++ src/library/Cuerpo.java
@@ -1,20 +1,53 @@
-package library;
+package crowly.library;
 
-import java.util.*;
+import java.util.Date;
+import java.util.UUID;
 
-/**
- * 
- */
 public class Cuerpo {
+	private int PosX;
+	private int PosY;
+	private Date Time;
+	private UUID Id;
+	
+	public Cuerpo(int pX, int pY, Date videoTime)
+	{
+		Id = UUID.randomUUID();
+		Time = videoTime;
+		PosY = pY;
+		PosX = pX;
+	}
 
-	private Date time;
-    private int PosX;
-    private int PosY;
-    private UUID Id; // UUID general unid identifier
-    
-    public Cuerpo(int pX, int pY, Date videoTime) {
-    	
-    	
-    }
-    
+	public int getPosX() {
+		return PosX;
+	}
+
+	public void setPosX(int posX) {
+		PosX = posX;
+	}
+
+	public int getPosY() {
+		return PosY;
+	}
+
+	public void setPosY(int posY) {
+		PosY = posY;
+	}
+
+	public Date getTime() {
+		return Time;
+	}
+
+	public void setTime(Date time) {
+		this.Time = Time;
+	}
+
+	public UUID getId() {
+		return Id;
+	}
+
+	public void setId(UUID id) {
+		Id = id;
+	}
+	
+	
 }
